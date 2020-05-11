a04fa763ee0eb21ef65a3c9df8e9902285483621
Ruiqi Mao <ruiqimao@google.com>
2020-05-10 22:48:23 -0700

    Add 8 bit arithmetic operations

--- gb/instructions.go
+++ gb/instructions.go
@@ -1,9 +1,5 @@
 package gb
 
-import (
-	"github.com/ruiqimao/go-gb-emu/utils"
-)
-
 // An Instruction returns how many cycles it takes to execute.
 type Instruction func() int
 
@@ -416,40 +412,523 @@ func (gb *GameBoy) createInstructionSet() {
 			return 16
 		},
 		0xf8: func() int { // LD HL,SP+r8.
-			r, flags := opSignedAdd(cpu.Sp(), cpu.IncPc())
-			cpu.Set16(RegHL, r)
-			cpu.Set(RegF, flags)
+			cpu.Set16(RegHL, cpu.opSignedAdd(cpu.Sp(), cpu.IncPc()))
 			return 12
 		},
 		0xf9: func() int { // LD SP,HL.
 			cpu.SetSp(cpu.Get16(RegHL))
 			return 8
 		},
+
+		// 8 bit arithmetic.
+		0x04: func() int { // INC B.
+			cpu.Set(RegB, cpu.opInc(cpu.Get(RegB)))
+			return 4
+		},
+		0x05: func() int { // DEC B.
+			cpu.Set(RegB, cpu.opDec(cpu.Get(RegB)))
+			return 4
+		},
+		0x0c: func() int { // INC C.
+			cpu.Set(RegC, cpu.opInc(cpu.Get(RegC)))
+			return 4
+		},
+		0x0d: func() int { // DEC C.
+			cpu.Set(RegC, cpu.opDec(cpu.Get(RegC)))
+			return 4
+		},
+		0x14: func() int { // INC D.
+			cpu.Set(RegD, cpu.opInc(cpu.Get(RegD)))
+			return 4
+		},
+		0x15: func() int { // DEC D.
+			cpu.Set(RegD, cpu.opDec(cpu.Get(RegD)))
+			return 4
+		},
+		0x1c: func() int { // INC E.
+			cpu.Set(RegE, cpu.opInc(cpu.Get(RegE)))
+			return 4
+		},
+		0x1d: func() int { // DEC E.
+			cpu.Set(RegE, cpu.opDec(cpu.Get(RegE)))
+			return 4
+		},
+		0x24: func() int { // INC H.
+			cpu.Set(RegH, cpu.opInc(cpu.Get(RegH)))
+			return 4
+		},
+		0x25: func() int { // DEC H.
+			cpu.Set(RegH, cpu.opDec(cpu.Get(RegH)))
+			return 4
+		},
+		0x27: func() int { // DAA.
+			cpu.Set(RegA, cpu.opDaa())
+			return 4
+		},
+		0x2c: func() int { // INC L.
+			cpu.Set(RegL, cpu.opInc(cpu.Get(RegL)))
+			return 4
+		},
+		0x2d: func() int { // DEC L.
+			cpu.Set(RegL, cpu.opDec(cpu.Get(RegL)))
+			return 4
+		},
+		0x2f: func() int { // CPL.
+			cpu.Set(RegA, ^cpu.Get(RegA))
+			cpu.SetFlag(FlagN, true)
+			cpu.SetFlag(FlagH, true)
+			return 4
+		},
+		0x34: func() int { // INC (HL).
+			hl := cpu.Get16(RegHL)
+			mem.Write(hl, cpu.opInc(mem.Read(hl)))
+			return 12
+		},
+		0x35: func() int { // DEC (HL).
+			hl := cpu.Get16(RegHL)
+			mem.Write(hl, cpu.opDec(mem.Read(hl)))
+			return 12
+		},
+		0x37: func() int { // SCF.
+			cpu.SetFlag(FlagN, false)
+			cpu.SetFlag(FlagH, false)
+			cpu.SetFlag(FlagC, true)
+			return 4
+		},
+		0x3c: func() int { // INC A.
+			cpu.Set(RegA, cpu.opInc(cpu.Get(RegA)))
+			return 4
+		},
+		0x3d: func() int { // DEC A.
+			cpu.Set(RegA, cpu.opDec(cpu.Get(RegA)))
+			return 4
+		},
+		0x3f: func() int { // CCF.
+			cpu.SetFlag(FlagN, false)
+			cpu.SetFlag(FlagH, false)
+			cpu.SetFlag(FlagC, !cpu.GetFlag(FlagC))
+			return 4
+		},
+		0x80: func() int { // ADD A,B.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegB), 0))
+			return 4
+		},
+		0x81: func() int { // ADD A,C.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegC), 0))
+			return 4
+		},
+		0x82: func() int { // ADD A,D.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegD), 0))
+			return 4
+		},
+		0x83: func() int { // ADD A,E.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegE), 0))
+			return 4
+		},
+		0x84: func() int { // ADD A,H.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegH), 0))
+			return 4
+		},
+		0x85: func() int { // ADD A,L.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegL), 0))
+			return 4
+		},
+		0x86: func() int { // ADD A,(HL).
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL)), 0))
+			return 8
+		},
+		0x87: func() int { // ADD A,A.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegA), 0))
+			return 4
+		},
+		0x88: func() int { // ADC A,B.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegB), 1))
+			return 4
+		},
+		0x89: func() int { // ADC A,C.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegC), 1))
+			return 4
+		},
+		0x8a: func() int { // ADC A,D.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegD), 1))
+			return 4
+		},
+		0x8b: func() int { // ADC A,E.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegE), 1))
+			return 4
+		},
+		0x8c: func() int { // ADC A,H.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegH), 1))
+			return 4
+		},
+		0x8d: func() int { // ADC A,L.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegL), 1))
+			return 4
+		},
+		0x8e: func() int { // ADC A,(HL).
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL)), 1))
+			return 8
+		},
+		0x8f: func() int { // ADC A,A.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.Get(RegA), 1))
+			return 4
+		},
+		0x90: func() int { // SUB B.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegB), 0))
+			return 4
+		},
+		0x91: func() int { // SUB C.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegC), 0))
+			return 4
+		},
+		0x92: func() int { // SUB D.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegD), 0))
+			return 4
+		},
+		0x93: func() int { // SUB E.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegE), 0))
+			return 4
+		},
+		0x94: func() int { // SUB H.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegH), 0))
+			return 4
+		},
+		0x95: func() int { // SUB L.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegL), 0))
+			return 4
+		},
+		0x96: func() int { // SUB (HL).
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL)), 0))
+			return 8
+		},
+		0x97: func() int { // SUB A.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegA), 0))
+			return 4
+		},
+		0x98: func() int { // SBC A,B.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegB), 1))
+			return 4
+		},
+		0x99: func() int { // SBC A,C.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegC), 1))
+			return 4
+		},
+		0x9a: func() int { // SBC A,D.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegD), 1))
+			return 4
+		},
+		0x9b: func() int { // SBC A,E.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegE), 1))
+			return 4
+		},
+		0x9c: func() int { // SBC A,H.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegH), 1))
+			return 4
+		},
+		0x9d: func() int { // SBC A,L.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegL), 1))
+			return 4
+		},
+		0x9e: func() int { // SBC A,(HL).
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL)), 1))
+			return 8
+		},
+		0x9f: func() int { // SBC A,A.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.Get(RegA), 1))
+			return 4
+		},
+		0xa0: func() int { // AND B.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegB)))
+			return 4
+		},
+		0xa1: func() int { // AND C.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegC)))
+			return 4
+		},
+		0xa2: func() int { // AND D.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegD)))
+			return 4
+		},
+		0xa3: func() int { // AND E.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegE)))
+			return 4
+		},
+		0xa4: func() int { // AND H.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegH)))
+			return 4
+		},
+		0xa5: func() int { // AND L.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegL)))
+			return 4
+		},
+		0xa6: func() int { // AND (HL).
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL))))
+			return 8
+		},
+		0xa7: func() int { // AND A.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.Get(RegA)))
+			return 4
+		},
+		0xa8: func() int { // XOR B.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegB)))
+			return 4
+		},
+		0xa9: func() int { // XOR C.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegC)))
+			return 4
+		},
+		0xaa: func() int { // XOR D.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegD)))
+			return 4
+		},
+		0xab: func() int { // XOR E.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegE)))
+			return 4
+		},
+		0xac: func() int { // XOR H.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegH)))
+			return 4
+		},
+		0xad: func() int { // XOR L.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegL)))
+			return 4
+		},
+		0xae: func() int { // XOR (HL).
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL))))
+			return 8
+		},
+		0xaf: func() int { // XOR A.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.Get(RegA)))
+			return 4
+		},
+		0xb0: func() int { // OR B.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegB)))
+			return 4
+		},
+		0xb1: func() int { // OR C.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegC)))
+			return 4
+		},
+		0xb2: func() int { // OR D.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegD)))
+			return 4
+		},
+		0xb3: func() int { // OR E.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegE)))
+			return 4
+		},
+		0xb4: func() int { // OR H.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegH)))
+			return 4
+		},
+		0xb5: func() int { // OR L.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegL)))
+			return 4
+		},
+		0xb6: func() int { // OR (HL).
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL))))
+			return 8
+		},
+		0xb7: func() int { // OR A.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.Get(RegA)))
+			return 4
+		},
+		0xb8: func() int { // CP B.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegB)))
+			return 4
+		},
+		0xb9: func() int { // CP C.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegC)))
+			return 4
+		},
+		0xba: func() int { // CP D.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegD)))
+			return 4
+		},
+		0xbb: func() int { // CP E.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegE)))
+			return 4
+		},
+		0xbc: func() int { // CP H.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegH)))
+			return 4
+		},
+		0xbd: func() int { // CP L.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegL)))
+			return 4
+		},
+		0xbe: func() int { // CP (HL).
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), mem.Read(cpu.Get16(RegHL))))
+			return 8
+		},
+		0xbf: func() int { // CP A.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.Get(RegA)))
+			return 4
+		},
+		0xc6: func() int { // ADD A,d8.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.IncPc(), 0))
+			return 8
+		},
+		0xce: func() int { // ADC A,d8.
+			cpu.Set(RegA, cpu.opAdd(cpu.Get(RegA), cpu.IncPc(), 1))
+			return 8
+		},
+		0xd6: func() int { // SUB d8.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.IncPc(), 0))
+			return 8
+		},
+		0xde: func() int { // SBC A,d8.
+			cpu.Set(RegA, cpu.opSub(cpu.Get(RegA), cpu.IncPc(), 1))
+			return 8
+		},
+		0xe6: func() int { // AND d8.
+			cpu.Set(RegA, cpu.opAnd(cpu.Get(RegA), cpu.IncPc()))
+			return 8
+		},
+		0xee: func() int { // XOR d8.
+			cpu.Set(RegA, cpu.opXor(cpu.Get(RegA), cpu.IncPc()))
+			return 8
+		},
+		0xf6: func() int { // OR d8.
+			cpu.Set(RegA, cpu.opOr(cpu.Get(RegA), cpu.IncPc()))
+			return 8
+		},
+		0xfe: func() int { // CP d8.
+			cpu.Set(RegA, cpu.opCp(cpu.Get(RegA), cpu.IncPc()))
+			return 8
+		},
 	}
 }
 
-// Perform an add and return the result and updated flags.
-func opAdd(a uint8, b uint8, c uint8) (uint8, uint8) {
-	r16 := uint16(a) + uint16(b) + uint16(c)
+// Perform an add, update flags, and return the result.
+func (c *Cpu) opAdd(a uint8, b uint8, cy uint8) uint8 {
+	r16 := uint16(a) + uint16(b) + uint16(cy)
 	r := uint8(r16)
 
-	flags := uint8(0)
-	flags = utils.SetBit(flags, FlagZ, r == 0)
-	flags = utils.SetBit(flags, FlagN, false)
-	flags = utils.SetBit(flags, FlagH, (a & 0xf) + (b & 0xf) + (c & 0xf) > 0xf)
-	flags = utils.SetBit(flags, FlagC, r16 > 0xff)
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, false)
+	c.SetFlag(FlagH, (a & 0xf) + (b & 0xf) + (cy & 0xf) > 0xf)
+	c.SetFlag(FlagC, r16 > 0xff)
 
-	return r, flags
+	return r
 }
 
-// Perform a signed add and return the result and updated flags.
-func opSignedAdd(a uint16, b uint8) (uint16, uint8) {
+// Perform a subtract, update flags, and return the result.
+func (c *Cpu) opSub(a uint8, b uint8, bw uint8) uint8 {
+	r16 := uint16(a) - uint16(b) - uint16(bw)
+	r := uint8(r16)
+
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, true)
+	c.SetFlag(FlagH, (a & 0xf) - (b & 0xf) - (bw & 0xf) > 0xf)
+	c.SetFlag(FlagC, r16 > 0xff)
+
+	return r
+}
+
+// Perform a signed add, update flags, and return the result.
+func (c *Cpu) opSignedAdd(a uint16, b uint8) uint16 {
 	r := uint16(int32(a) + int32(int8(b)))
 
 	// Get the flags from doing an ordinary add.
-	_, flags := opAdd(uint8(a), b, 0)
-	flags = utils.SetBit(flags, FlagZ, false)
-	flags = utils.SetBit(flags, FlagN, false)
+	c.opAdd(uint8(a), b, 0)
+	c.SetFlag(FlagZ, false)
+	c.SetFlag(FlagN, false)
 
-	return r, flags
+	return r
+}
+
+// Perform an increment, update flags, and return the result.
+func (c *Cpu) opInc(v uint8) uint8 {
+	// Do not update the carry flag.
+	cy := c.GetFlag(FlagC)
+	r := c.opAdd(v, 1, 0)
+	c.SetFlag(FlagC, cy)
+	return r
+}
+
+// Perform a decrement, update flags, and return the result.
+func (c *Cpu) opDec(v uint8) uint8 {
+	// Do not update the carry flag.
+	cy := c.GetFlag(FlagC)
+	r := c.opSub(v, 1, 0)
+	c.SetFlag(FlagC, cy)
+	return r
+}
+
+// Perform decimal adjust on register A, update flags, and return the result.
+func (c *Cpu) opDaa() uint8 {
+	a := c.Get(RegA)
+
+	// Stolen from https://forums.nesdev.com/viewtopic.php?t=15944#p196282.
+	if !c.GetFlag(FlagN) {
+		if c.GetFlag(FlagH) || (a & 0xf) > 9 {
+			a += 0x06
+		}
+		if c.GetFlag(FlagC) || a > 0x99 {
+			a += 0x60
+			c.SetFlag(FlagC, true)
+		}
+	} else {
+		if c.GetFlag(FlagC) {
+			a -= 0x60
+		}
+		if c.GetFlag(FlagH) {
+			a -= 0x6
+		}
+	}
+
+	c.SetFlag(FlagZ, a == 0)
+	c.SetFlag(FlagH, false)
+
+	return a
+}
+
+
+// Perform an AND operation, update flags, and return the result.
+func (c *Cpu) opAnd(a uint8, b uint8) uint8 {
+	r := a & b
+
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, false)
+	c.SetFlag(FlagH, true)
+	c.SetFlag(FlagC, false)
+
+	return r
+}
+
+// Perform an XOR operation, update flags, and return the result.
+func (c *Cpu) opXor(a uint8, b uint8) uint8 {
+	r := a ^ b
+
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, false)
+	c.SetFlag(FlagH, false)
+	c.SetFlag(FlagC, false)
+
+	return r
+}
+
+// Perform an OR operation, update flags, and return the result.
+func (c *Cpu) opOr(a uint8, b uint8) uint8 {
+	r := a | b
+
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, false)
+	c.SetFlag(FlagH, false)
+	c.SetFlag(FlagC, false)
+
+	return r
+}
+
+// Perform a CP operation, update flags, and return the result.
+func (c *Cpu) opCp(a uint8, b uint8) uint8 {
+	r := a - b
+
+	c.SetFlag(FlagZ, r == 0)
+	c.SetFlag(FlagN, true)
+	c.SetFlag(FlagH, a & 0xf < b & 0xf)
+	c.SetFlag(FlagC, a < b)
+
+	return r
 }
