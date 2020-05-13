ruiqimao/go-gb-emu

commit bac20608797482dd9398b0e91dcaaa101d6c97f2

Add framework for PPU

gb/memory.go
@@ -31,6 +31,20 @@ import (
 //   High RAM.
 // FFFF
 //   Interrupts enable register.
+const (
+	AddrBoot = 0x0000
+	AddrCartROM0 = 0x0000
+	AddrCartROMN = 0x4000
+	AddrVRAM = 0x8000
+	AddrCartRAM = 0xa000
+	AddrWRAM0 = 0xc000
+	AddrWRAMN = 0xd000
+	AddrEcho = 0xe000
+	AddrOAM = 0xfe00
+	AddrEmpty = 0xfea0
+	AddrIO = 0xff00
+	AddrHRAM = 0xff80
+)
 
 type Memory struct {
 	gb *GameBoy
@@ -63,50 +77,50 @@ func (m *Memory) Read(addr uint16) uint8 {
 		return m.gb.boot[addr]
 
 	// Cartridge ROM bank 0.
-	case addr < 0x4000:
+	case addr < AddrCartROMN:
 		// TODO.
 		return 0x00
 
 	// Cartridge ROM bank 1 - N.
-	case addr < 0x8000:
+	case addr < AddrVRAM:
 		// TODO.
 		return 0x00
 
 	// Video RAM.
-	case addr < 0xa000:
-		return m.vram[addr-0x8000]
+	case addr < AddrCartRAM:
+		return m.vram[addr-AddrVRAM]
 
 	// Cartridge RAM.
-	case addr < 0xc000:
+	case addr < AddrWRAM0:
 		// TODO.
 		return 0x00
 
 	// Work RAM banks 0 and 1.
-	case addr < 0xe000:
-		return m.wram[addr-0xc000]
+	case addr < AddrEcho:
+		return m.wram[addr-AddrWRAM0]
 
 	// Mirror of C000 - DDFF.
-	case addr < 0xfe00:
+	case addr < AddrOAM:
 		return m.Read(addr - 0x2000)
 
 	// Sprite attribute table.
-	case addr < 0xfea0:
-		return m.oam[addr-0xfe00]
+	case addr < AddrEmpty:
+		return m.oam[addr-AddrOAM]
 
 	// Empty.
-	case addr < 0xff00:
+	case addr < AddrIO:
 		return 0x00
 
 	// I/O registers.
-	case addr < 0xff80:
+	case addr < AddrHRAM:
 		return m.ReadIO(addr)
 
 	// High RAM.
-	case addr < 0xffff:
-		return m.hram[addr-0xff80]
+	case addr < AddrIE:
+		return m.hram[addr-AddrIO]
 
 	// Interrupt enable register.
-	case addr == 0xffff:
+	case addr == AddrIE:
 		return m.ie
 
 	}
@@ -120,35 +134,35 @@ func (m *Memory) Write(addr uint16, v uint8) {
 	switch {
 
 	// Video RAM.
-	case addr >= 0x8000 && addr < 0xa000:
-		m.vram[addr-0x8000] = v
+	case addr >= AddrVRAM && addr < AddrCartRAM:
+		m.vram[addr-AddrVRAM] = v
 
 	// Cartridge RAM.
-	case addr >= 0xa000 && addr < 0xc000:
+	case addr >= AddrCartRAM && addr < AddrWRAM0:
 		// TODO.
 
 	// Work RAM banks 0 and 1.
-	case addr >= 0xc000 && addr < 0xe000:
-		m.wram[addr-0xc000] = v
+	case addr >= AddrWRAM0 && addr < AddrEcho:
+		m.wram[addr-AddrWRAM0] = v
 
 	// Mirror of C000 - DDFF.
-	case addr >= 0xe000 && addr < 0xfe00:
+	case addr >= AddrEcho && addr < AddrOAM:
 		m.Write(addr-0x2000, v)
 
 	// Sprite attribute table.
-	case addr >= 0xfe00 && addr < 0xfea0:
-		m.oam[addr-0xfe00] = v
+	case addr >= AddrOAM && addr < AddrIO:
+		m.oam[addr-AddrOAM] = v
 
 	// I/O registers.
-	case addr >= 0xff00 && addr < 0xff80:
+	case addr >= AddrIO && addr < AddrHRAM:
 		m.WriteIO(addr, v)
 
 	// High RAM.
-	case addr >= 0xff80 && addr < 0xffff:
-		m.hram[addr-0xff80] = v
+	case addr >= AddrHRAM && addr < AddrIE:
+		m.hram[addr-AddrHRAM] = v
 
 	// Interrupt enable register.
-	case addr == 0xffff:
+	case addr == AddrIE:
 		m.ie = v
 
 	}

gb/ppu.go
@@ -1,0 +1,126 @@
+package gb
+
+// LCDC flags.
+const (
+	FlagLcdPower      = 7
+	FlagWindowTileMap = 6
+	FlagWindowEnable  = 5
+	FlagBgTileset     = 4
+	FlagBgTileMap     = 3
+	FlagSpriteSize    = 2
+	FlagSpritesEnable = 1
+	FlagBgEnable      = 0
+)
+
+// STAT flags.
+const (
+	FlagLYCCheck      = 6
+	FlagMode2Check    = 5
+	FlagMode1Check    = 4
+	FlagMode0Check    = 3
+	FlagLYCComparison = 2
+	ModeHBlank        = 0
+	ModeVBlank        = 1
+	ModeOAM           = 2
+	ModeTransfer      = 3
+)
+
+// Pixel processing unit.
+type Ppu struct {
+	gb *GameBoy
+
+	sc uint16 // Scanline counter.
+	ly uint8 // Current scanline.
+
+	lcdc uint8 // LCD control.
+	stat uint8 // LCD status.
+}
+
+func NewPpu(gb *GameBoy) *Ppu {
+	p := &Ppu{
+		gb: gb,
+	}
+	return p
+}
+
+// Catch the PPU up to the CPU.
+func (p *Ppu) Update(cycles int) {
+	// Process 4 cycles at a time.
+	for cycles > 0 {
+
+		// Update the mode.
+		// This is done at the beginning of the step because mode changes are delayed by a step.
+		switch {
+		case p.ly < 144 && p.sc == 0:
+			p.setMode(ModeOAM)
+		case p.ly < 144 && p.sc == 20:
+			p.setMode(ModeTransfer)
+		case p.ly < 144 && p.sc == 63:
+			p.setMode(ModeHBlank)
+		case p.ly == 144 && p.sc == 0:
+			p.setMode(ModeVBlank)
+		}
+
+		// If the PPU is not in a blanking mode, execute a step.
+		mode := p.Mode()
+		if mode == ModeOAM {
+			p.stepOAM()
+		}
+		if mode == ModeTransfer {
+			p.stepTransfer()
+		}
+
+		// Increment the scanline counter and scanline.
+		p.sc = (p.sc + 1) % 114
+		if p.sc == 0 {
+			p.ly = (p.ly + 1) % 154
+		}
+
+	}
+}
+
+// Execute a step of OAM searching.
+func (p *Ppu) stepOAM() {
+	// TODO.
+}
+
+// Execute a step of pixel transfer.
+func (p *Ppu) stepTransfer() {
+	// TODO.
+}
+
+// Set the mode.
+func (p *Ppu) setMode(m uint8) {
+	p.stat = p.stat & 0xf8 | m & 0x03
+}
+
+// Get the value of LCDC.
+func (p *Ppu) LCDC() uint8 {
+	return p.lcdc
+}
+
+// Set the value of LCDC.
+func (p *Ppu) SetLCDC(v uint8) {
+	p.lcdc = v
+}
+
+// Get the value of STAT.
+func (p *Ppu) STAT() uint8 {
+	return p.lcdc
+}
+
+// Set the value of STAT.
+func (p *Ppu) SetSTAT(v uint8) {
+	// Only set writable values.
+	p.stat = p.stat & 0x0f | v & 0xf0
+}
+
+// Get the mode.
+func (p *Ppu) Mode() uint8 {
+	return p.stat & 0x03
+}
+
+// Get the value of LY.
+func (p *Ppu) LY() uint8 {
+	return p.ly
+}

gb/gameboy.go
@@ -12,6 +12,7 @@ const (
 
 type GameBoy struct {
 	cpu *Cpu
+	ppu *Ppu
 	mem *Memory
 
 	clk *Clock
@@ -28,6 +29,7 @@ func NewGameBoy() (*GameBoy, error) {
 
 	// Create the components.
 	gb.cpu = NewCpu(gb)
+	gb.ppu = NewPpu(gb)
 	gb.mem = NewMemory(gb)
 	gb.clk = NewClock(BaseClock)
 
@@ -63,6 +65,9 @@ func (gb *GameBoy) RunCycles(limit int) int {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// Catch the PPU up to the CPU.
+		gb.ppu.Update(cycles)
 
 		limit -= cycles
 	}

gb/interrupts.go
@@ -97,6 +97,7 @@ func (c *Cpu) updateTimers(cycles int) {
 		tima := c.gb.mem.Read(AddrTIMA)
 
 		// If there was an overflow last cycle, reset TIMA.
+		// This ignores any writes to TIMA that may have occurred during this cycle.
 		if c.of {
 			// Reset TIMA to TMA.
 			tima = c.gb.mem.Read(AddrTMA)

gb/io.go
@@ -80,10 +80,22 @@ func (m *Memory) ReadIO(addr uint16) uint8 {
 
 	case AddrIF:
 		// Upper 3 bits are always high.
-		return m.io[addr-0xff00] | 0xe0
+		return m.io[addr-AddrIO] | 0xe0
+
+	case AddrLCDC:
+		// Retrieve from PPU.
+		return m.gb.ppu.LCDC()
+
+	case AddrSTAT:
+		// Retrieve from PPU.
+		return m.gb.ppu.STAT()
+
+	case AddrLY:
+		// Retrieve from PPU.
+		return m.gb.ppu.LY()
 
 	default:
-		return m.io[addr-0xff00]
+		return m.io[addr-AddrIO]
 
 	}
 }
@@ -107,10 +119,21 @@ func (m *Memory) WriteIO(addr uint16, v uint8) {
 
 	case AddrTAC:
 		// Only lower 3 bits are writable.
-		m.io[addr-0xff00] = v & 0x07
+		m.io[addr-AddrIO] = v & 0x07
+
+	case AddrLCDC:
+		// Redirect to PPU.
+		m.gb.ppu.SetLCDC(v)
+
+	case AddrSTAT:
+		// Redirect to PPU.
+		m.gb.ppu.SetSTAT(v)
+
+	case AddrLY:
+		// Read only.
 
 	default:
-		m.io[addr-0xff00] = v
+		m.io[addr-AddrIO] = v
 
 	}
 }

commit 5313f90b2f52fed37f48c2b2108a6558b44586c5

Fix typo introduced in bac20608797482dd9398b0e91dcaaa101d6c97f2

gb/memory.go
@@ -32,18 +32,18 @@ import (
 // FFFF
 //   Interrupts enable register.
 const (
-	AddrBoot = 0x0000
+	AddrBoot     = 0x0000
 	AddrCartROM0 = 0x0000
 	AddrCartROMN = 0x4000
-	AddrVRAM = 0x8000
-	AddrCartRAM = 0xa000
-	AddrWRAM0 = 0xc000
-	AddrWRAMN = 0xd000
-	AddrEcho = 0xe000
-	AddrOAM = 0xfe00
-	AddrEmpty = 0xfea0
-	AddrIO = 0xff00
-	AddrHRAM = 0xff80
+	AddrVRAM     = 0x8000
+	AddrCartRAM  = 0xa000
+	AddrWRAM0    = 0xc000
+	AddrWRAMN    = 0xd000
+	AddrEcho     = 0xe000
+	AddrOAM      = 0xfe00
+	AddrEmpty    = 0xfea0
+	AddrIO       = 0xff00
+	AddrHRAM     = 0xff80
 )
 
 type Memory struct {
@@ -54,7 +54,7 @@ type Memory struct {
 
 	// Scratch space for I/O registers.
 	// IE flag is stored in last byte of scratch space.
-	IO   [0x81]uint8
+	IO [0x81]uint8
 }
 
 func NewMemory(gb *GameBoy) *Memory {
@@ -88,7 +88,7 @@ func (m *Memory) Read(addr uint16) uint8 {
 
 	// Video RAM.
 	case addr < AddrCartRAM:
-		return m.gb.ppu.ReadVRAM(addr-AddrVRAM)
+		return m.gb.ppu.ReadVRAM(addr - AddrVRAM)
 
 	// Cartridge RAM.
 	case addr < AddrWRAM0:
@@ -105,7 +105,7 @@ func (m *Memory) Read(addr uint16) uint8 {
 
 	// Sprite attribute table.
 	case addr < AddrEmpty:
-		return m.gb.ppu.ReadOAM(addr-AddrOAM)
+		return m.gb.ppu.ReadOAM(addr - AddrOAM)
 
 	// Empty.
 	case addr < AddrIO:
@@ -117,7 +117,7 @@ func (m *Memory) Read(addr uint16) uint8 {
 
 	// High RAM.
 	case addr < AddrIE:
-		return m.hram[addr-AddrIO]
+		return m.hram[addr-AddrHRAM]
 
 	// Interrupt enable register.
 	case addr == AddrIE:

gb/oam.go
@@ -4,4 +4,3 @@ package gb
 func (p *PPU) stepOAM() {
 	// TODO.
 }
-

gb/ppu.go
@@ -33,7 +33,7 @@ const (
 
 // Buffer constants.
 const (
-	FrameWidth = 160
+	FrameWidth  = 160
 	FrameHeight = 144
 )
 
@@ -42,7 +42,7 @@ type PPU struct {
 	gb *GameBoy
 
 	sc uint16 // Scanline counter.
-	ly uint8 // Current scanline.
+	ly uint8  // Current scanline.
 
 	lcdc uint8 // LCD control.
 	stat uint8 // LCD status.
@@ -52,7 +52,7 @@ type PPU struct {
 	oam  [0x100]uint8
 
 	// Current frame.
-	frame [FrameWidth*FrameHeight]uint8
+	frame [FrameWidth * FrameHeight]uint8
 }
 
 func NewPPU(gb *GameBoy) *PPU {
@@ -101,7 +101,7 @@ func (p *PPU) Update(cycles int) {
 
 // Set the mode.
 func (p *PPU) setMode(m uint8) {
-	p.stat = p.stat & 0xf8 | m & 0x03
+	p.stat = p.stat&0xf8 | m&0x03
 }
 
 // Read a byte from the VRAM.
@@ -154,7 +154,7 @@ func (p *PPU) STAT() uint8 {
 // Set the value of STAT.
 func (p *PPU) SetSTAT(v uint8) {
 	// Only set writable values.
-	p.stat = p.stat & 0x0f | v & 0xf0
+	p.stat = p.stat&0x0f | v&0xf0
 }
 
 // Get the mode.

gb/transfer.go
@@ -17,4 +17,3 @@ func (p *PPU) pushFrame() {
 	default:
 	}
 }
-

main/display.go
@@ -16,8 +16,8 @@ type Display struct {
 	window *glfw.Window
 
 	// Graphics objects.
-	program *gfx.Program // Shader program.
-	quad *gfx.Vao // Quad shape.
+	program *gfx.Program   // Shader program.
+	quad    *gfx.Vao       // Quad shape.
 	texture *gfx.Texture2D // Display texture.
 }
 
@@ -27,7 +27,7 @@ func NewDisplay() (*Display, error) {
 
 	// Initialize the window.
 	var err error
-	d.window, err = gfx.NewWindow(DisplayScale * gb.FrameWidth, DisplayScale * gb.FrameHeight, "Game Boy", false)
+	d.window, err = gfx.NewWindow(DisplayScale*gb.FrameWidth, DisplayScale*gb.FrameHeight, "Game Boy", false)
 	if err != nil {
 		return nil, err
 	}